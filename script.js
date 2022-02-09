f1af973c58f12b682f6796b09f3242a27673057a
Kitorosano <rosanoesteban2@gmail.com>
2022-02-09 11:31:13 -0300

    Mejora a la solucion con comprobaciones (ya fuera de la actividad)

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let matriz = [];
-nroPrados = 4;
+let matriz = [[0,1], [1,2], [2,0], [3,4]];
+nroPrados = 5;
 
 function solucion(nroPrados, matriz){
-  if(nroPrados >= 1 && nroPrados <= Math.pow(10,5)){
+  // Comprobar si el nroPrado es valido
+  if(nroPrados < 1 || nroPrados > Math.pow(10,5)) return alert("El numero de prado debe de estar contenido entre 0 y 10^5");
+  // Comprobar que la cantidad de filas es valida
+  if(matriz.length < 0 || matriz.length > Math.pow(10,5)) return alert("La matriz solo puede tener entre 0 a 10^5 filas");
 
-    return (nroPrados - matriz.length) - 1;
-  } else {
-    console.log("El numero de prado es invalido, porfavor que este entre el 1 y el 100000");
-    return
+  
+  // Recorrer cada fila y cada columna de la matriz para comprobar...
+  for( let fila of matriz){
+    for(let columna of fila){
+      // Comprobar que los valores de la matriz son numeros validos
+      if(typeof columna !== "number") return alert("La matriz solo puede tener 2 columnas.")
+      if(columna < 0 || columna > nroPrados -1) return alert("Los valores de la matriz son invalidos.")
+    }
   }
+
+  return (nroPrados - matriz.length) - 1;
 }
